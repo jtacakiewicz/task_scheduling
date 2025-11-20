e7d51116b3b75ba35d3767d4f4586c589d961609
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2025-11-20 10:37:23 +0100

    02: test: script enchance colors

--- 02/test.sh
+++ 02/test.sh
@@ -4,14 +4,16 @@ for idx in 155975; do
     echo "=============== Solver ${idx} ==============="
     for x in 50 100 150 200 250 300 350 400 450 500; do
         echo ">>>>>> Instance ${x}"
-        ./${idx}* in/in_155963_${x}.txt  out/out.txt
+        { time ./${idx}* in/in_155963_${x}.txt  out/out.txt; } 2> /tmp/result${idx}_${x}.txt
+        time=`head /tmp/result${idx}_${x}.txt -n 2 | tail -n 1 | sed 's/.*real.*.m//g'`
+        echo -e "\e[34mTime taken: ${time}\e[0m"
         python valid.py in/in_155963_${x}.txt  out/out.txt
         result=${?}
         if [[ $result == 1 ]]; then
-            echo "ERRORS DETECTED!"
+            echo -e "\e[31mERRORS DETECTED!\e[0m"
         else
             val=$(head -n 1 out/out.txt)
-            echo "U for ${idx};${x}:    $val"
+            echo -e "\e[32mU for ${idx};${x}:    $val\e[0m"
         fi
 
         echo "<<<<<<"
