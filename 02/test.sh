5275babfc1ba2ba5565152160c790ebe48a5612e
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2025-11-20 13:47:29 +0100

    02: test: all indexes

--- 02/test.sh
+++ 02/test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-for idx in 155975; do
+
+
+for idx in 154978 155826 155851 155866 155868 155891 155892 155905 155913 155954 155958 155963 155967 155975 156010 156029; do; do
     echo "=============== Solver ${idx} ==============="
     for x in 50 100 150 200 250 300 350 400 450 500; do
         echo ">>>>>> Instance ${x}"
-        { time ./${idx}* in/in_155963_${x}.txt  out/out.txt; } 2> /tmp/result${idx}_${x}.txt
+        { time ./solvers/${idx}* in/in_155963_${x}.txt  out/out.txt $((x / 10)); } 2> /tmp/result${idx}_${x}.txt
         time=`head /tmp/result${idx}_${x}.txt -n 2 | tail -n 1 | sed 's/.*real.*.m//g'`
         echo -e "\e[34mTime taken: ${time}\e[0m"
         python valid.py in/in_155963_${x}.txt  out/out.txt
